0ea50b8712ee7a203e108f9af736a288b4a8fccf
Mike Calvanese <mfcalvanese@gmail.com>
2018-12-25 10:42:23 -0500

    refactor prop reducer tests for accounts and tokenBalance

--- app/reducers/accounts.test.js
+++ app/reducers/accounts.test.js
@@ -1,41 +1,13 @@
-import assert from 'assert'
 import { accountsLoaded } from '../actions'
 import accounts from './accounts'
+import runSimplePropTest from '../test/runSimplePropTest'
 
-describe('accounts', () => {
-  describe('default', () => {
-    describe('when given undefined state', () => {
-      it('should return empty array', () => {
-        assert.deepEqual(
-          accounts(undefined, { type: 'UNKNOWN' }),
-          []
-        )
-      })
-    })
-  })
+const mockAccounts = ['0x123', '0x456']
 
-  describe('accountsLoaded', () => {
-    [
-      {
-        when: 'when given undefined state',
-        should: 'should return accounts array',
-        state: undefined,
-        action: accountsLoaded({ accounts: ['0x123', '0x456'] }),
-        expected: ['0x123', '0x456']
-      },
-      {
-        when: 'when given an array of accounts',
-        should: 'should overwrite and return new accounts array',
-        state: ['0x789'],
-        action: accountsLoaded({ accounts: ['0x123', '0x456'] }),
-        expected: ['0x123', '0x456']
-      }
-    ].forEach(({ when, should, state, action, expected }) => {
-      describe(when, () => {
-        it(should, () => {
-          assert.deepEqual(accounts(state, action), expected)
-        })
-      })
-    })
-  })
+runSimplePropTest({
+  reducer: accounts,
+  action: accountsLoaded({ accounts: mockAccounts }),
+  propName: 'accounts',
+  defaultValue: [],
+  expectedValue: mockAccounts
 })
--- app/reducers/tokenBalance.test.js
+++ app/reducers/tokenBalance.test.js
@@ -1,41 +1,13 @@
-import assert from 'assert'
 import { tokenBalanceLoaded } from '../actions'
 import tokenBalance from './tokenBalance'
+import runSimplePropTest from '../test/runSimplePropTest'
 
-describe('tokenBalance', () => {
-  describe('default', () => {
-    describe('when given undefined state', () => {
-      it('should return null', () => {
-        assert.equal(
-          tokenBalance(undefined, { type: 'UNKNOWN' }),
-          null
-        )
-      })
-    })
-  })
+const mockBalance = 123
 
-  describe('tokenBalanceLoaded', () => {
-    [
-      {
-        when: 'when given undefined state',
-        should: 'should return balance',
-        state: undefined,
-        action: tokenBalanceLoaded({ balance: 123 }),
-        expected: 123
-      },
-      {
-        when: 'when given a balance',
-        should: 'should overwrite and return new balance',
-        state: 123,
-        action: tokenBalanceLoaded({ balance: 456 }),
-        expected: 456
-      }
-    ].forEach(({ when, should, state, action, expected }) => {
-      describe(when, () => {
-        it(should, () => {
-          assert.deepEqual(tokenBalance(state, action), expected)
-        })
-      })
-    })
-  })
+runSimplePropTest({
+  reducer: tokenBalance,
+  action: tokenBalanceLoaded({ balance: mockBalance }),
+  propName: 'tokenBalance',
+  defaultValue: null,
+  expectedValue: mockBalance
 })
