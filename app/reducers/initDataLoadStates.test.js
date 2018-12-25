4027d1a91341cc83297c8c0640f1fe5601db33ed
Mike Calvanese <mfcalvanese@gmail.com>
2018-12-25 10:21:56 -0500

    refactor init data loading tests

--- app/reducers/initDataLoadStates.test.js
+++ app/reducers/initDataLoadStates.test.js
@@ -7,108 +7,83 @@ import {
 } from '../actions'
 import initDataLoadStates from './initDataLoadStates'
 
+const testSpecs = [
+  {
+    prop: 'accounts',
+    actionName: 'accountsLoaded',
+    action: accountsLoaded({ accounts: ['0x123'] }),
+    errorActionName: 'accountsLoadingError',
+    errorAction: accountsLoadingError({ errorMessage: 'mock_error' })
+  },
+  {
+    prop: 'tokenBalance',
+    actionName: 'tokenBalanceLoaded',
+    action: tokenBalanceLoaded({ balance: 'mock_balance' }),
+    errorActionName: 'tokenBalanceLoadingError',
+    errorAction: tokenBalanceLoadingError({ errorMessage: 'mock_error' })
+  }
+]
+
 describe('initDataLoadStates', () => {
   describe('default', () => {
     describe('when given undefined state', () => {
       it('should return default state', () => {
+        let expectedDefault = {}
+        testSpecs.forEach(spec => {
+          expectedDefault[spec.prop] = {
+            loaded: false,
+            errorMessage: null
+          }
+        })
         assert.deepEqual(
           initDataLoadStates(undefined, { type: 'UNKNOWN' }),
-          {
-            accounts: {
-              loaded: false,
-              errorMessage: null
-            },
-            tokenBalance: {
-              loaded: false,
-              errorMessage: null
-            }
-          }
+          expectedDefault
         )
       })
     })
   })
 
-  describe('accountsLoaded', () => {
-    [
-      {
-        when: 'when given undefined state',
-        should: 'should set accounts.loaded = true',
-        state: undefined,
-        action: accountsLoaded({ accounts: ['0x123'] }),
-        expected: { errorMessage: null, loaded: true }
-      },
-      {
-        when: 'when given state with an errorMessage',
-        should: 'should clear the errorMessage',
-        state:  { loaded: false, errorMessage: 'mock_error' },
-        action: accountsLoaded({ accounts: ['0x123'] }),
-        expected: { errorMessage: null, loaded: true }
-      }
-    ].forEach(({ when, should, state, action, expected }) => {
-      describe(when, () => {
-        it(should, () => {
-          assert.deepEqual(initDataLoadStates(state, action).accounts, expected)
+  // test the `loaded` and `errorMessage` properties  
+  testSpecs.forEach(({ actionName, action, errorActionName, errorAction, prop }) => {
+    describe(actionName, () => {
+      [
+        {
+          when: 'when given undefined state',
+          should: `should set loaded = true`,
+          state: undefined,
+          action,
+          expected: { errorMessage: null, loaded: true }
+        },
+        {
+          when: 'when given state with an errorMessage',
+          should: 'should clear the errorMessage',
+          state:  { loaded: false, errorMessage: 'mock_error' },
+          action,
+          expected: { errorMessage: null, loaded: true }
+        }
+      ].forEach(({ when, should, state, action, expected }) => {
+        describe(when, () => {
+          it(should, () => {
+            assert.deepEqual(initDataLoadStates(state, action)[prop], expected)
+          })
         })
       })
     })
-  })
 
-  describe('tokenBalanceLoaded', () => {
-    [
-      {
-        when: 'when given undefined state',
-        should: 'should set tokenBalance.loaded = true',
-        state: undefined,
-        action: tokenBalanceLoaded({ balance: 'mock_balance' }),
-        expected: { errorMessage: null, loaded: true }
-      },
-      {
-        when: 'when given state with an errorMessage',
-        should: 'should clear the errorMessage',
-        state:  { loaded: false, errorMessage: 'mock_error' },
-        action: tokenBalanceLoaded({ balance: 'mock_balance' }),
-        expected: { errorMessage: null, loaded: true }
-      }
-    ].forEach(({ when, should, state, action, expected }) => {
-      describe(when, () => {
-        it(should, () => {
-          assert.deepEqual(initDataLoadStates(state, action).tokenBalance, expected)
-        })
-      })
-    })
-  })
-
-  describe('accountsLoadingError', () => {
-    [
-      {
-        when: 'when given undefined state',
-        should: 'should set accounts.errorMessage',
-        state: undefined,
-        action: accountsLoadingError({ errorMessage: 'mock_error' }),
-        expected: { errorMessage: 'mock_error', loaded: false }
-      }
-    ].forEach(({ when, should, state, action, expected }) => {
-      describe(when, () => {
-        it(should, () => {
-          assert.deepEqual(initDataLoadStates(state, action).accounts, expected)
-        })
-      })
-    })
-  })
-
-  describe('tokenBalanceLoadingError', () => {
-    [
-      {
-        when: 'when given undefined state',
-        should: 'should set tokenBalance.errorMessage',
-        state: undefined,
-        action: tokenBalanceLoadingError({ errorMessage: 'mock_error' }),
-        expected: { errorMessage: 'mock_error', loaded: false }
-      }
-    ].forEach(({ when, should, state, action, expected }) => {
-      describe(when, () => {
-        it(should, () => {
-          assert.deepEqual(initDataLoadStates(state, action).tokenBalance, expected)
+    describe(errorActionName, () => {
+      [
+        {
+          when: 'when given undefined state',
+          should: 'should set errorMessage',
+          state: undefined,
+          errorAction,
+          expected: { errorMessage: 'mock_error', loaded: false }
+        }
+      ].forEach(({ when, should, state, errorAction, expected }) => {
+        describe(when, () => {
+          it(should, () => {
+            assert.deepEqual(initDataLoadStates(state, errorAction)[prop], expected)
+          })
         })
       })
     })
