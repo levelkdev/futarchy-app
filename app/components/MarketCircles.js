4d1e816c3c79188c1bf84b535c3f3f64b45b45de
Mike Calvanese <mfcalvanese@gmail.com>
2019-01-10 15:03:58 -0500

    fix the circle math so the circle angle changes with the size

--- app/components/MarketCircles.js
+++ app/components/MarketCircles.js
@@ -14,9 +14,15 @@ const MarketCircles = ({
   yesPercentage,
   noPercentage
 }) => {
-  // TODO: get this working at all angles, write some formula for semi-randomizing
-  const angle = 45
-  const circleDiameters = calcCircleDiameters(yesPercentage, noPercentage)
+  let y = yesPercentage
+  let n = noPercentage
+
+  const { yes: yesDiam, no: noDiam } = calcCircleDiameters(y, n)
+
+  const angle = 
+    ((yesDiam - noDiam) / (yesDiam + noDiam))
+       * (Math.PI / 3)
+       * (1 / (1 - (2 * CIRCLE_MIN_RELATIVE_PERCENTAGE)))
 
   return (
     <div>
@@ -24,14 +30,14 @@ const MarketCircles = ({
         <circle
           cx={OUTER_CIRCLE_RADIUS}
           cy={OUTER_CIRCLE_RADIUS}
-          r={OUTER_CIRCLE_RADIUS - 2}
+          r={OUTER_CIRCLE_RADIUS}
           stroke="#e8e8e8"
           stroke-width="2"
           fill="transparent"
         />
         <Circle
           angle={angle}
-          diameter={circleDiameters.yes}
+          diameter={yesDiam}
           color="#80aedc"
           nameText="YES"
           isYes={true}
@@ -39,7 +45,7 @@ const MarketCircles = ({
         />
         <Circle
           angle={angle}
-          diameter={circleDiameters.no}
+          diameter={noDiam}
           color="#38cad0"
           nameText="NO"
           isYes={false}
@@ -51,15 +57,9 @@ const MarketCircles = ({
 }
 
 const Circle = ({ angle, diameter, color, nameText, priceText, isYes }) => {
-  const radians = toRadians(angle)
-
-  const magicNumber = 8
-
-  const radius = diameter / 2 - 1
-  const virtualRadius = circleOffset(diameter, isYes)
-  const virtualOrigin = (OUTER_CIRCLE_RADIUS - magicNumber) / Math.PI
-  const xOffset = virtualOrigin + virtualRadius * Math.cos(radians)
-  const yOffset = virtualOrigin + virtualRadius * Math.sin(radians)
+  const radius = diameter / 2
+  const xOffset = OUTER_CIRCLE_RADIUS - (OUTER_CIRCLE_RADIUS - radius) * (isYes ? 1 : -1) * Math.cos(angle)
+  const yOffset = OUTER_CIRCLE_RADIUS - (OUTER_CIRCLE_RADIUS - radius) * (isYes ? 1 : -1) * Math.sin(angle)
 
   let nameFontSize = Math.round(CIRCLE_NAME_MAX_FONT_SIZE * (radius / OUTER_CIRCLE_RADIUS))
   if (nameFontSize < MIN_FONT_SIZE) nameFontSize = MIN_FONT_SIZE
@@ -86,16 +86,11 @@ const Circle = ({ angle, diameter, color, nameText, priceText, isYes }) => {
   )
 }
 
-function circleOffset(circleDiameter, offsetNegative) {
-  const offsetSign = (offsetNegative ? -1 : 1)
-  return OUTER_CIRCLE_RADIUS + ((OUTER_CIRCLE_DIAMETER - circleDiameter) / 2) * offsetSign - (3 * offsetSign)
-}
-
 function calcCircleDiameters(yesPercentage, noPercentage) {
   const relativePercentages = calcRelativePercentages(yesPercentage, noPercentage)
   return {
-    yes: OUTER_CIRCLE_DIAMETER * relativePercentages.yes - 2,
-    no: OUTER_CIRCLE_DIAMETER * relativePercentages.no - 2
+    yes: OUTER_CIRCLE_DIAMETER * relativePercentages.yes,
+    no: OUTER_CIRCLE_DIAMETER * relativePercentages.no
   }
 }
 
@@ -105,16 +100,16 @@ function calcRelativePercentages(yesPercentage, noPercentage) {
     yes: .5 + (diff / 2),
     no: .5 - (diff / 2)
   }
-  if(relativePercentages.yes < CIRCLE_MIN_RELATIVE_PERCENTAGE) {
-    const adjustmentDiff = CIRCLE_MIN_RELATIVE_PERCENTAGE - relativePercentages.yes
-    relativePercentages.yes = CIRCLE_MIN_RELATIVE_PERCENTAGE
-    relativePercentages.no = relativePercentages.no - adjustmentDiff
+  for (var i = 0; i < 2; i++) {
+    const val = i == 0 ? 'yes' : 'no'
+    const oppositeVal = i == 0 ? 'no' : 'yes'
+    if(relativePercentages[val] < CIRCLE_MIN_RELATIVE_PERCENTAGE) {
+      const adjustmentDiff = CIRCLE_MIN_RELATIVE_PERCENTAGE - relativePercentages[val]
+      relativePercentages[val] = CIRCLE_MIN_RELATIVE_PERCENTAGE
+      relativePercentages[oppositeVal] = relativePercentages[oppositeVal] - adjustmentDiff
+    }
   }
   return relativePercentages
-}
-
-function toRadians (angle) {
-  return angle * (Math.PI / 180);
 }
 
 export default MarketCircles
