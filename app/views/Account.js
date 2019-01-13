499d23eadcf1ba643e5115c0eec4f51130e9a432
Mike Calvanese <mfcalvanese@gmail.com>
2019-01-13 17:50:10 -0500

    output some raw performance data on the UI
    this is just a placeholder view for the "my accounts" view, but it's helpful for debugging for now

---
+++ app/views/Account.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PerformanceSummaryContainer from '../containers/PerformanceSummaryContainer'
+
+const Account = () => (
+  <div>
+    <PerformanceSummaryContainer />
+  </div>
+)
+
+export default Account
