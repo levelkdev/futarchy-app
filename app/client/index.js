5b1d8dc6b56985dfa6ffe3a344ba2fdcde2fe3b4
Mike Calvanese <mfcalvanese@gmail.com>
2019-02-12 17:39:26 -0800

    remove calcCosts function

--- app/client/index.js
+++ app/client/index.js
@@ -62,13 +62,6 @@ export const decisions = async (decisionId) => {
   return decision
 }
 
-export const calcCosts = async (decisionId, outcomeTokenAmounts) => {
-  const [ yesShort, yesLong, noShort, noLong ] = await call(
-    'calcCosts', decisionId, outcomeTokenAmounts
-  )
-  return { yesShort, yesLong, noShort, noLong }
-}
-
 export const traderDecisionBalances = async (decisionId, trader) => {
   const hash = traderDecisionHash(trader, decisionId)
   const balances = await call('traderDecisionBalances', hash)
@@ -187,7 +180,6 @@ export default {
   fee,
   tradingPeriod,
   marketFundAmount,
-  calcCosts,
   traderDecisionBalances,
   newDecision,
   buyMarketPositions,
