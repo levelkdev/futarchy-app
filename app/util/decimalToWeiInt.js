f6563fcae9b65bb097507bd06b91f9b7a712af41
Emily Williams <emily.williams@levelk.io>
2019-03-11 16:12:01 -0400

    Format big numbers correctly on buyMarketPositions ui

--- app/util/decimalToWeiInt.js
+++ app/util/decimalToWeiInt.js
@@ -1,11 +1,8 @@
-import DecimalJS from 'decimal.js'
-let Decimal = DecimalJS.clone({ precision: 80, toExpPos: 9999 })
+import BigNumber from 'bignumber.js';
 
 // TODO: this really should be tested
 function decimalToWeiInt (val) {
-  const d = new Decimal(val.toString())
-  // * 10^18 to get the wei value
-  return d.mul(new Decimal('1000000000000000000')).round().toString()
+  return web3.toWei(val, 'ether')
 }
 
 export default decimalToWeiInt
