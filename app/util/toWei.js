b1652e478a03a03f79196d324014d61c8cc671a6
Mike Calvanese <mfcalvanese@gmail.com>
2019-05-21 16:17:17 -0400

    validate collateral balance amount in make prediction form
    fixes https://github.com/levelkdev/futarchy-app/issues/106

--- app/util/toWei.js
+++ app/util/toWei.js
@@ -1,6 +1,9 @@
 const web3Utils = require('web3-utils')
 
 function toWei (val, units) {
+  if (val === '' || typeof(val) === 'undefined' || val === null) {
+    val = '0'
+  }
   return web3Utils.toWei(val, units)
 }
 
